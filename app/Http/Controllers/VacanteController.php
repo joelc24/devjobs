ed98dae98e8cf3b74aa72058d36f13a9cd425760
Joel cues <libra-marriaga@hotmail.com>
2022-12-06 10:29:05 -0500

    Manejar roles dev y reclutador

--- app/Http/Controllers/VacanteController.php
+++ app/Http/Controllers/VacanteController.php
@@ -14,7 +14,8 @@ class VacanteController extends Controller
      */
     public function index()
     {
-        //
+        $this->authorize('viewAny', Vacante::class);
+        $this->authorize('create', Vacante::class);
         return view('vacantes.index');
     }
 
